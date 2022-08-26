fdb2a54a0f6508dc12731e50efc2e8f260c46201
pylloldi <pylloldi@gmail.com>
2022-08-26 16:02:51 -0500

    barra de menu responsive

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,19 +6,6 @@ Perfil {{$user->username}}
 
 @section('contenido')
 
-<div class="flex items-center gap-2">
-    <p>Perfil: {{$user->username}}</p>
-    @auth
-        @if ($user->id === auth()->user()->id)
-        <a href="{{route('profile.index')}}" class="text-gray-500 hover:text-gray-600 cursor-pointer">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                <path d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z" />
-            </svg>
-        </a>            
-        @endif
-    @endauth
-</div>
-
 <section class="container mx-auto mt-10">
     @if($posts->count())
         <div class="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
